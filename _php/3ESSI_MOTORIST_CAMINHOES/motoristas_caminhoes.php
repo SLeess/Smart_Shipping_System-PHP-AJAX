2f0e888a20961dd512e16bed66f7506b9cb1d1af
SLeess <duraesleandro12@gmail.com>
2023-11-28 17:02:58 -0300

    Update - Responsive Notas

--- _php/3ESSI_MOTORIST_CAMINHOES/motoristas_caminhoes.php
+++ _php/3ESSI_MOTORIST_CAMINHOES/motoristas_caminhoes.php
@@ -91,7 +91,15 @@
                 <?php
                     $title = "- Cadastro de Motoristas e Caminhões";
                     require_once("../elements/tituloProjetoMainSection.php");
-                ?>
+                ?><div class="row">
+                <div class="col-sm-5 col-md-5 col-lg-4 col-5">
+                    <span>Qtd de notas selecionadas:</span>
+                    <input id="qtdLinhas" class="form-control" type="text" name="" value="0" disabled="" style="width: 50px;">
+                </div>
+                <div class="col-sm-6 offset-sm-1 col-md-5 offset-md-2 col-5 offset-2 col-lg-5 offset-lg-3">
+                    <button id="btnSelecionarTodas" class="btn btn-outline-primary col-md-auto">Selecionar todas as linhas abaixo</button>
+                </div>
+            </div>
                 <div class="px-2 py-2" style="margin-bottom: 35px;">
                     <div class="row g-4 py-2 col-md-12">
                         <form action="gravarMotorista.php" method="POST" class="d-flex">
