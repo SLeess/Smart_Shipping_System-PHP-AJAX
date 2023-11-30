f556e6de49e23b4ad5f3316505746b31c051c2f9
Leandro <duraesleandro12@gmail.com>
2023-11-30 12:06:13 -0300

    Update

--- _php/elements/navbarDD.php
+++ _php/elements/navbarDD.php
@@ -12,20 +12,29 @@ echo "<ul class='nav nav-tabs nav-pills flex-col flex-sm-row flex-md-row' id='my
     "<a class='nav-link' id='home-tab' data-toggle='tab' href='".$relative."home.php' role='tab' aria-controls='home' aria-selected='false' style='height: 100%'>Home</a>
   </li>";
 
-if($_SESSION['tipo'] == 1)
-  echo "<li class='nav-item dropdown'>
-    <a class='nav-link dropdown-toggle' style='padding-bottom: 15px;' id='notas-dropdown' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Notas</a>
+if($_SESSION['tipo'] == 1){
+  echo "<li class='nav-item dropdown' style='width: 160px;'>
+    <a class='nav-link dropdown-toggle' style='padding-bottom: 13px;' id='notas-dropdown' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Notas</a>
     <div class='dropdown-menu' style='background-color: white; padding: 5px 0;'>
-      <a class='dropdown-item' id='notas-nmonit-tab' href='".$relative."3ESSI_NOTAS_MONITORAMENTO/notas.php'>Buscar Notas</a>
-      <a class='dropdown-item' id='notas-tab' href='".$relative."3SSI_CRUD/notas.php'>Inserir Notas</a>
+      <a class='dropdown-item' id='notas-nmonit-tab' href='".$relative."3ESSI_NOTAS_MONITORAMENTO/notas.php'>Buscar</a>
+      <a class='dropdown-item' id='notas-tab' href='".$relative."3SSI_CRUD/notas.php'>Inserir</a>
     </div>
   </li>";
+  echo "<li class='nav-item dropdown' style='width: 160px;'>
+    <a class='nav-link dropdown-toggle' style='padding-bottom: 13px;' id='notas-dropdown' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Motoristas</a>
+    <div class='dropdown-menu' style='background-color: white; padding: 5px 0;'>
+      <a class='dropdown-item' id='motoristas-tab' href='".$relative."3ESSI_MOTORIST_CAMINHOES/motoristas_caminhoes.php' role='tab' aria-controls='Inserir' aria-selected='false'>Cadastrar</a>
+      <a class='dropdown-item' id='notas-tab' href='".$relative."#'>Visualizar</a>
+    </div>
+  </li>";
+}
+
+//<li class='nav-item'>
+// <a class='nav-link' id='motoristas-tab' data-toggle='tab' href='".$relative."3ESSI_MOTORIST_CAMINHOES/motoristas_caminhoes.php' role='tab' aria-controls='Motoristas e Caminhoes' aria-selected='false' style='height: 100%'>Motoristas & Caminhões</a>
+// </li>
 
 echo
   "<li class='nav-item'>
-    <a class='nav-link' id='motoristas-tab' data-toggle='tab' href='".$relative."3ESSI_MOTORIST_CAMINHOES/motoristas_caminhoes.php' role='tab' aria-controls='Motoristas e Caminhoes' aria-selected='false' style='height: 100%'>Motoristas & Caminhões</a>
-  </li>
-  <li class='nav-item'>
     <a class='nav-link' id='mapa-tab' data-toggle='tab' href='".$relative."3ESSI_MAPA_CARREGAMENTO/view_get_products.php' role='tab' aria-controls='Mapa de Carregamentos' aria-selected='false' style='height: 100%'>Mapa de Carregamento</a>
   </li>
   <li class='flex-sm-fill nav-item'>
