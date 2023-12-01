ecc3bc7f62e3c471ae852c6b18e7d518389fade6
SLeess <duraesleandro12@gmail.com>
2023-12-01 09:50:16 -0300

    Em andamento - Nav bar

--- _php/elements/E_navbar.php
+++ _php/elements/E_navbar.php
@@ -9,26 +9,22 @@ $relative = isset($_POST['relative']) ? $_POST['relative'] : '';
 
   echo '<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">3S</a>
+    <a class="navbar-brand" href="'.$relative.'home.php">3S</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav" style="width: 100%;">';
-        
-      '<li class=" nav-item">'.
-      '<a class="nav-link" id="home-tab" data-toggle="tab" href="'.$relative.'home.php" role="tab" aria-controls="home" aria-selected="false" style="height: 100%">Home</a>
-    </li>';
   
   if($_SESSION["tipo"] == 1){
-    echo '<li class=" nav-item dropdown">
+    echo '<li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" style="padding-bottom: 13px;" id="notas-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Notas</a>
       <div class="dropdown-menu" style="background-color: white; padding: 5px 0;">
         <a class="dropdown-item" id="notas-nmonit-tab" href="'.$relative.'3ESSI_NOTAS_MONITORAMENTO/view_get_notas.php">Buscar</a>
         <a class="dropdown-item" id="notas-tab" href="'.$relative.'3SSI_CRUD/view_set_notas.php">Inserir</a>
       </div>
     </li>';
-    echo '<li class=" nav-item dropdown">
+    echo '<li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" style="padding-bottom: 13px;" id="notas-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Motoristas</a>
       <div class="dropdown-menu" style="background-color: white; padding: 5px 0;">
         <a class="dropdown-item" id="motoristas-tab" href="'.$relative.'3ESSI_MOTORIST_CAMINHOES/view_set_motorista.php" role="tab" aria-controls="Inserir" aria-selected="false">Cadastrar</a>
@@ -37,30 +33,30 @@ $relative = isset($_POST['relative']) ? $_POST['relative'] : '';
     </li>';
   } else{
     echo 
-    '<li class=" nav-item">'.
+    '<li class="nav-item">'.
       '<a class="nav-link" id="notas-nmonit-tab" data-toggle="tab" href="'.$relative.'3ESSI_NOTAS_MONITORAMENTO/view_get_notas.php" role="tab" aria-controls="home" aria-selected="false" style="height: 100%">Buscar Notas</a>
     </li>';
     echo 
-    '<li class=" nav-item">'.
+    '<li class="nav-item">'.
       '<a class="nav-link" id="motoristas-view-tab" data-toggle="tab" href="'.$relative.'" role="tab" aria-controls="home" aria-selected="false" style="height: 100%">Visualizar Motoristas</a>
     </li>';
   }
   
   echo
-    '<li class=" nav-item">
+    '<li class="nav-item">
       <a class="nav-link" id="mapa-tab" data-toggle="tab" href="'.$relative.'3ESSI_MAPA_CARREGAMENTO/view_get_products.php" role="tab" aria-controls="Mapa de Carregamentos" aria-selected="false" style="height: 100%">Mapa de Carregamento</a>
     </li>';
     
   if($_SESSION["tipo"] == 1){
     echo 
-    '<li class=" nav-item">'.
+    '<li class="nav-item">'.
       '<a class="nav-link" id="perfis-tab" data-toggle="tab" href="'.$relative.'view_get_perfis.php" role="tab" aria-controls="home" aria-selected="false" style="height: 100%">Perfis cadastros</a>
     </li>';
   }
     
   echo '<li class="nav-item ms-auto">
       <a class="nav-link" id="profile-tab" data-toggle="tab" href="'.$relative.'view_perfil_configs.php" role="tab" aria-controls="Clientes" aria-selected="false" style="height: 100%">
-          <img src="https://cdn-icons-png.flaticon.com/256/6596/6596121.png" alt="" width="32" height="32" class="rounded-circle me-2">';
+          <img src="https://cdn-icons-png.flaticon.com/256/6596/6596121.png" alt="" width="28" height="28" class="rounded-circle me-2">';
     if($_SESSION['tipo'] == 1){
       echo '<strong style="color: red;">'.$_SESSION["usuario"].'
           </strong>';
--- _php/home.php
+++ _php/home.php
@@ -14,12 +14,13 @@
     <link rel="stylesheet" href="../_style/style.css">
     <!-- <link rel="stylesheet" href="../_style/navbar.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="../_scriptjs/script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+    <script src="../_scriptjs/script.js"></script>
     <style>
         .nav-pills .nav-link.active{
             background-color: #042ba3;
         }
+
         <?php echo "nav{
             background-color: #333;
             margin-bottom: 2em;
@@ -35,10 +36,6 @@
             padding: 15px;
             display: inline-block;
             transition: all 0.5s;
-        }
-
-        nav li a:hover{
-            background-color: red;
         }
 
         .dropdown-menu{
@@ -107,8 +104,8 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="../_scriptjs/script.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../_scriptjs/script.js"></script>
     <script>
         $(document).ready(() => {
             $.ajax({
